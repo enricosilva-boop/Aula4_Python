34979877bb9cfcb1b6b4ad4e0f73897322d7577d
enrico_souza <enrico.silva@ort.org.br>
2026-02-25 15:39:22 -0300

    Exe 04

---
+++ Exe04.py
@@ -0,0 +1,23 @@
+'''
+4. Escreva um programa que receba um número e apresente a sua tabuada.
+Exemplo: 
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50
+'''
+
+#ENTRADA
+numero = (int(input('Informe seu numero')))
+
+#PROCESSAMENTO
+print('x 1:', numero * 1)
+print('x 2:', numero * 2)
+print('x 3:', numero * 3)
+print('x 4:', numero * 4)
+print('x 5:', numero * 5)
+print('x 6:', numero * 6)
+print('x 7:', numero * 7)
+print('x 8:', numero * 8)
+print('x 9:', numero * 9)
+print('x 10:', numero * 10)
